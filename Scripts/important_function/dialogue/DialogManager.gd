47ea639462635935dfcbf68d87efe4c4692e4dbc
SamuelPoi348 <samanniep@gmail.com>
2026-03-25 10:26:29 -0400

    ajout du marchand, sa première intéraction

--- Scripts/important_function/dialogue/DialogManager.gd
+++ Scripts/important_function/dialogue/DialogManager.gd
@@ -22,29 +22,33 @@ func start_dialog(position: Vector2, lines: Array[String]):
 	is_dialog_active = true
 	
 func _show_text_box():
-	text_box = text_box_scene.instantiates()
+	text_box = text_box_scene.instantiate()
 	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
 	get_tree().root.add_child(text_box)
 	text_box.global_position = text_box_position
-	text_box.display_text(dialog_lines)
+	
+	text_box.display_text(dialog_lines[current_line_index])
 	can_advance_line = false
 	
 func _on_text_box_finished_displaying():
 	can_advance_line = true
 	
 func _unhandled_input(event: InputEvent):
-	if(
-		event.is_action_pressed("advance_dialog") &&
+	if (
+		event.is_action_pressed("interagir") &&
 		is_dialog_active &&
 		can_advance_line
 	):
 		text_box.queue_free()
 		
-		current_line_index +=1
+		current_line_index += 1
+		
 		if current_line_index >= dialog_lines.size():
 			is_dialog_active = false
-			current_line_index =0
+			current_line_index = 0
 			return
+		
+		_show_text_box()
 	
 	
 	
