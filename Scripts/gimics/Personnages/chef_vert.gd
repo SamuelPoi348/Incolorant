6e1a7ed00a6b35798817f8d0e51be9e2a9957394
SamuelPoi348 <samanniep@gmail.com>
2026-03-30 10:07:24 -0400

    ajout de scène pour les personnages

---
+++ Scripts/gimics/Personnages/chef_vert.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var player_in_range = false
+var chef_vert_dialog_active: Array[String] =[
+	"Tu dois être le nouveau, celui que le marchand à mentionné s'être réveillé.",
+	"Je suis le chef de se village de la forêt",
+	"Malheureusement je ne peut pas vraiment te dire qui tu est ni pourquoi tu est là,",
+	"mais je peut t'expliquer un peu, ce qui se passe sur cette île.",
+	"Il y a quatres tribus distincte soit la mienne dans cette forêt,",
+	"la bleu dans la montagne, la jaune dans le desert et la rouge dans la terre volcanique.",
+	"Les autres tribu pense que la montagne au milieu de l'île serait là ou se trouve",
+	"le dragon endromi qui aurait été célé par nos ancêtre chef de tribu.",
+	"Ils pensent que ce dragon pourrait bientôt se réveiller et certain se prépare,",
+	"en vu de cette évantualité les autres tribu sont un peu à cran.",
+	""
+]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
