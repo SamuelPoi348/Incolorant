b355d6be424c56c0ffd4f99ea4593b6403e0535d
SamuelPoi348 <samanniep@gmail.com>
2026-04-06 13:02:17 -0400

    frefonte des tiles pour le volcan rouge

--- Scripts/important_function/menu_principal.gd
+++ Scripts/important_function/menu_principal.gd
@@ -2,6 +2,7 @@ extends Control
 @onready var test = preload("res://Scenes/ScenePresentation.tscn")
 @onready var l1 = preload("res://Scenes/Zones/Plage_Grise/PG-1.tscn")
 @onready var FV1 = preload("res://Scenes/Zones/Foret_Verte/FV-3.tscn")
+@onready var VR1 = preload("res://Scenes/Zones/Volcan_Rouge/VR-1.tscn")
 
 @onready var option = preload("res://Scenes/ui/option.tscn")
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +12,7 @@ func _ready() -> void:
 
 func _on_commencer_button_down() -> void:
 	await get_tree().create_timer(1).timeout
-	get_tree().root.get_node("Main").change_scene(FV1)
+	get_tree().root.get_node("Main").change_scene(VR1)
 
 
 func _on_option_button_down() -> void:
