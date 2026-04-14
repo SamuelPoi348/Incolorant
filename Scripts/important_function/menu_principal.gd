767c10eff1f2dfbe9d3c1652921a963019fed956
MileshPatel28 <milesh.patel28@gmail.com>
2026-04-14 12:56:37 -0400

    MNT-B1 level complete

--- Scripts/important_function/menu_principal.gd
+++ Scripts/important_function/menu_principal.gd
@@ -1,5 +1,5 @@
 extends Control
-@onready var test = preload("res://Scenes/Zones/Volcan_Rouge/VR-2.tscn")
+@onready var test = preload("res://Scenes/Zones/Mont_Bleu/MB-1.tscn")
 @onready var l1 = preload("res://Scenes/Zones/Plage_Grise/PG-1.tscn")
 #@onready var FV1 = preload("res://Scenes/Zones/Foret_Verte/FV-1.tscn")
 @onready var VR1 = preload("res://Scenes/Zones/Volcan_Rouge/VR-3.tscn")
