532e14778f8e3e9c10c2a860e23c38e974526cd2
SamuelPoi348 <samanniep@gmail.com>
2026-04-17 12:16:58 -0400

    upgrade du niveau mb-2

--- Scenes/gimics/Personnages/habitant_bleu.gd
+++ Scenes/gimics/Personnages/habitant_bleu.gd
@@ -2,15 +2,36 @@ extends Area2D
 
 @export var habitant_id: int = 0
 var visited = false
+var waiting_for_dialog_end=false
 
+var dialogues_random: Array = [
+	["Merciiii !!! Tu m'as trouvé !"] as Array[String],
+	["Youpiii ! Enfin quelqu’un !"] as Array[String],
+	["C’est fire 🔥 merci à toi !"] as Array[String]
+]
 func _ready() -> void:
 	add_to_group("Habitant_Bleu")
 	body_entered.connect(_on_body_entered)
+	DialogManager.dialog_finished.connect(_on_dialog_finished)
+	randomize()
 
 func _on_body_entered(body):
 	if body.is_in_group("Player") and not visited:
-		visited = true
-		print("Habitant bleu %d visited!" % habitant_id)
+		if !DialogManager.is_dialog_active:
+			visited = true
+			waiting_for_dialog_end = true
+		
+			var random_index = randi() % dialogues_random.size()
+			var chosen_dialog = dialogues_random[random_index]
+			
+			DialogManager.start_dialog(global_position + Vector2(50,50), chosen_dialog)
+			print("Habitant bleu %d visited!" % habitant_id)
+			
 
 func is_visited() -> bool:
 	return visited
+	
+func _on_dialog_finished():
+	if waiting_for_dialog_end:
+		waiting_for_dialog_end = false
+		visible = false
--- Scripts/gimics/Personnages/chef_bleu.gd
+++ Scripts/gimics/Personnages/chef_bleu.gd
@@ -10,17 +10,16 @@ var chef_bleu_dialog_active: Array[String] = [
 	" a quasiment été anéanti durant le dernier siècle.",
 	"Certe nous vivons longtemps mais nous ne somme pas imortel non plus,",
 	"il suffit de nous éteindre pour que notre âme quite notre corps.",
-	"Pour perpétuer mon espèce je dois rentrer en contact d'autre personne,",
-	"maniant une couleur différente de la miène.",
-	"Tu possède une îcone verte!! Tu pourrais m'aider alors?",
-	"Il te suffit de partager ton histoire et alors le foyer de la renaissance,",
+	"Pour perpétuer mon espèce nous devons acomplir le rituel des flames,",
+	"Tu possède déja une îcone Tu pourrais m'aider alors?",
+	"Il te suffit de retrouver les 5 habitants perdu et alors le foyer de la renaissance,",
 	"pourra se mettre en marche"
 ]
 var chef_bleu_dialogue_active_2: Array[String] = [
 	"Reste m'aider stp."
 ]
 var chef_bleu_dialogue_active_3: Array[String] = [
-	"Merci mon ami tu peut aller chercher l'îcone bleu elle pourrais te servir à l'avenir."
+	"Merci mon ami tu peut continuer ton voyage désormais la grande porte est ouverte"
 ]
 
 
@@ -33,9 +32,15 @@ func _process(delta: float) -> void:
 func _unhandled_input(event):
 	if event.is_action_pressed("interagir") and player_in_range:
 		if !DialogManager.is_dialog_active:
-			if !first_dialog_done:
+			
+			# 🔥 PRIORITÉ AU DIALOGUE FINAL
+			if tous_les_habitants_visites():
+				DialogManager.start_dialog(global_position + Vector2(50,50), chef_bleu_dialogue_active_3)
+			
+			elif !first_dialog_done:
 				DialogManager.start_dialog(global_position + Vector2(50,50), chef_bleu_dialog_active)
 				first_dialog_done = true
+			
 			else:
 				DialogManager.start_dialog(global_position + Vector2(50,50), chef_bleu_dialogue_active_2)
 
@@ -53,3 +58,12 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	player_in_range = false
 	DialogManager.dialog_lines= []
+	
+func tous_les_habitants_visites() -> bool:
+	var habitants = get_tree().get_nodes_in_group("Habitant_Bleu")
+	if habitants.is_empty():
+		return false
+	for h in habitants:
+		if not h.is_visited():
+			return false
+	return true
