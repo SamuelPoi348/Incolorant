593904aceb3dbbbc1771f9b226acafb8121c5813
MileshPatel28 <milesh.patel28@gmail.com>
2026-04-21 16:33:47 -0400

    platforme ghost

--- Scenes/gimics/plateform_jaune_default/movingPlatform.gd
+++ Scenes/gimics/plateform_jaune_default/movingPlatform.gd
@@ -5,19 +5,26 @@ enum PlatformState { AT_ORIGIN, AT_TARGET, MOVING }
 
 @export var move_offset: Vector2 = Vector2(0, -96)
 @export var move_speed: float = 100.0
+@export var showGhost: bool = false
 
 var origin_position: Vector2
 var target_position: Vector2
 var current_state: PlatformState = PlatformState.AT_ORIGIN
 var moving_to_target: bool = false
+var sprite_ignore_original_position: Vector2
 
 @onready var a = $Area2D
+@onready var sprite_ignore = $SpriteIgnore
 
 func _ready():
 	a.body_entered.connect(_on_a_body_entered)
 	origin_position = global_position
 	target_position = origin_position + move_offset
 	add_to_group("YellowPlatform")
+	
+	# Store original position of SpriteIgnore and set visibility
+	sprite_ignore_original_position = sprite_ignore.global_position
+	sprite_ignore.visible = showGhost
 
 func _physics_process(delta):
 	var main = get_tree().root.get_node("Main")
@@ -46,7 +53,13 @@ func _move_platform(delta):
 
 	var movement = direction.normalized() * move_speed * delta
 	global_position += movement
+	# Keep SpriteIgnore in its original position
+	sprite_ignore.global_position = sprite_ignore_original_position
 	
 func _on_a_body_entered(body):
+	# Ignore bodies marked with SpriteIgnore
+	if body.name.contains("SpriteIgnore"):
+		return
+	
 	if body is PlayerController:
 		body.die()
