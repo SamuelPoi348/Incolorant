cd0d12486ba6d595094cfa61d772ee459439a730
SamuelPoi348 <samanniep@gmail.com>
2026-04-27 15:57:26 -0400

    ajout de crédit ainsi que retour au menu ou map

--- Scripts/gimics/portail.gd
+++ Scripts/gimics/portail.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var next_scene = preload("res://Scenes/Zones/map/map.tscn")
 @export var spawn_position: Vector2 = Vector2.ZERO
 @export var niveau_termine : String = ""  # <-- chemin du niveau à marquer comme terminé
+@export var final_lvl: bool = false
 
 var player_inside = false
 var main
@@ -21,6 +22,10 @@ func _process(_delta):
 			main.marquer_niveau_complet(niveau_termine)
 
 		# Change la scène et place le joueur
+		if final_lvl:
+			show_credits()
+			get_tree().paused = true
+			return
 		var target_pos = global_position if spawn_position == Vector2.ZERO else spawn_position
 		main.scene_courante = "res://Scenes/Zones/map/map.tscn"
 		main.call_deferred("change_scene", next_scene, target_pos)
@@ -32,3 +37,10 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if body.name == "Player":
 		player_inside = false
+		
+func show_credits():
+	var credits_nodes = get_tree().get_nodes_in_group("credits")
+	
+	for c in credits_nodes:
+		if c.has_method("start_credits"):
+			c.start_credits()
