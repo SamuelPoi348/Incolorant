c99d33d78fc7fa7fbeb7b15252d309847bb42f98
SamuelPoi348 <samanniep@gmail.com>
2026-04-27 13:13:37 -0400

    nerf du pouvoir jaune sur le dragon

--- Scenes/gimics/dragon/segment_jaune.gd
+++ Scenes/gimics/dragon/segment_jaune.gd
@@ -2,22 +2,26 @@ extends StaticBody2D
 
 @onready var anim = $AnimatedSprite2D
 @onready var area_kill = $Area2D
+@onready var area_activation = $Area2D2
 
 var can_kill_ok := true
 var main
 var busy := false
-
+var player_in_zone := false
 
 func _ready():
 	area_kill.body_entered.connect(_on_body_entered)
 
+	area_activation.body_entered.connect(_on_activation_entered)
+	area_activation.body_exited.connect(_on_activation_exited)
 
 func _process(delta):
 	main = get_tree().root.get_node("Main")
 	
 	if main.couleur_active == "jaune":
-		if Input.is_action_just_pressed("switch_incolorant_mode"):
-			toggle_state()
+		if player_in_zone:
+			if Input.is_action_just_pressed("switch_incolorant_mode"):
+				toggle_state()
 
 
 func toggle_state():
@@ -48,5 +52,15 @@ func _on_body_entered(body):
 		if body.has_method("die"):
 			body.die()
 			
+			
+func _on_activation_entered(body):
+	if body.is_in_group("Player"):
+		player_in_zone = true
+
+func _on_activation_exited(body):
+	if body.is_in_group("Player"):
+		player_in_zone = false
+		
 func is_yellow_active():
 	return not can_kill_ok
+	
