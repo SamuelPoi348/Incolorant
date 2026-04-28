66c0309d27539db65c0d0c1e3b000c98102f73ce
SamuelPoi348 <samanniep@gmail.com>
2026-04-28 09:48:49 -0400

    problème de la porte

--- porte_rouge.gd
+++ porte_rouge.gd
@@ -4,10 +4,15 @@ var fermé_ok = true
 var récepteur_ok = true
 var first_timer = true
 var first_ouverture=true
+
+var est_fermee = false
+var cycle_termine = false
+
 @onready var anim = $AnimatedSprite2D
 @onready var collision = $CollisionShape2D
 @onready var c = $Area2D
 @onready var c4 = $Area2D2
+@export var premiere_porte: bool =false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	c.body_entered.connect(_on_c_body_entered)
@@ -16,8 +21,8 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if not fermé_ok:
-		if récepteur_ok && tous_les_recepteurs_remplis():
+	if est_fermee:
+		if récepteur_ok and tous_les_recepteurs_remplis():
 			ouvrir_toutes_les_portes()
 	
 func _on_c_body_entered(body):
@@ -25,28 +30,29 @@ func _on_c_body_entered(body):
 		body.die()
 		
 func _on_c4_body_entered(body):
-	if body is PlayerController and fermé_ok:
+	if body is PlayerController and not est_fermee and not cycle_termine and premiere_porte:
 		for porte in get_tree().get_nodes_in_group("Porte_Rouge"):
-			if porte.fermé_ok:
-				porte.fermé_ok = false
+			if not porte.est_fermee:
+				porte.est_fermee = true
 				porte.anim.play("fermé")
 				porte.collision.set_deferred("disabled", false)
-				# 👉 lancer le timer UNE SEULE FOIS
+
 		if first_timer:
-			first_timer=false
+			first_timer = false
 			get_tree().get_first_node_in_group("Niveau").lancer_timer()
 	
 func ouvrir_toutes_les_portes():
 	if first_ouverture:
-		first_ouverture=false
+		first_ouverture = false
+		cycle_termine = true # 🔥 IMPORTANT
 		for porte in get_tree().get_nodes_in_group("Porte_Rouge"):
 			porte.ouvrir()
-		récepteur_ok=false
+		récepteur_ok = false
 		
 func ouvrir():
-	if not fermé_ok:
-		fermé_ok = true
-		anim.play("ouvrir") # adapte au nom de ton anim
+	if est_fermee:
+		est_fermee = false
+		anim.play("ouvrir")
 		collision.set_deferred("disabled", true)
 					
 func tous_les_recepteurs_remplis() -> bool:
