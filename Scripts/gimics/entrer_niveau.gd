bc3bbd04e882d48cf6787f3b3dc875a8c999b0bd
SamuelPoi348 <samanniep@gmail.com>
2026-05-04 17:07:15 -0400

    ajout du case senstive pour la partie portail de la forêt

--- Scripts/gimics/entrer_niveau.gd
+++ Scripts/gimics/entrer_niveau.gd
@@ -25,7 +25,10 @@ func _ready():
 	_update_animation()
 
 func _update_animation():
+	print(chemin_niveau)
 	if chemin_niveau in main.niveaux_completes:
+			
+			print(main.niveaux_completes)
 			anim.play("complete")
 	else:
 			anim.play("default")
