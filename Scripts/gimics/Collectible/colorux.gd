bfbe929e095d3547e43d06cb795f28ba763fccb3
SamuelPoi348 <samanniep@gmail.com>
2026-05-07 14:59:02 -0400

    augmentation du nombre de sauvegarde dans le jeu à chaque récupération d'objet

--- Scripts/gimics/Collectible/colorux.gd
+++ Scripts/gimics/Collectible/colorux.gd
@@ -3,8 +3,10 @@ extends Area2D
 @export var id_colorux : String
 
 @onready var coin_sound = $CoinSound
+var main
 
 func _ready():
+	main = get_tree().root.get_node("Main")
 	var niveau = get_tree().current_scene.scene_file_path
 	coin_sound.bus = "SFX"
 	if get_tree().root.get_node("Main").colorux_deja_pris(niveau, id_colorux):
@@ -19,5 +21,6 @@ func _on_body_entered(body):
 		
 		# attendre la fin du son avant de supprimer
 		await coin_sound.finished
-		
+		if not main.admin:
+			main.sauvegarder()
 		queue_free()
